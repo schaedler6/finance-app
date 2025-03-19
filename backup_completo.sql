e18cc041855022078f3bd19f14da3bd18444b2c0
Gabriel Pires de Barros <gdbarros95@gmail.com>
2025-03-19 20:14:13 -0300

    Delete backup_completo.sql

--- backup_completo.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL
-);
-INSERT INTO usuarios (nome, email, senha) 
-VALUES ('Admin', 'admin@email.com', SHA2('123456', 256));
